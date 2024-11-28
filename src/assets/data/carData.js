32581ae7c7cccf8a57b50b88f032f833b04aaedf
Sakshi Verma <130901688+sakshiv-ermaa@users.noreply.github.com>
2024-11-28 09:31:40 +0530

    Add files via upload

---
+++ src/assets/data/carData.js
@@ -0,0 +1,141 @@
+// import all images from assets/images directory
+import img01 from "../all-images/cars-img/tata.png";
+import img02 from "../all-images/cars-img/creta.png";
+import img03 from "../all-images/cars-img/swift.png";
+import img04 from "../all-images/cars-img/nissan-offer.png";
+import img05 from "../all-images/cars-img/offer-toyota.png";
+import img06 from "../all-images/cars-img/mercedes-offer.png";
+import img07 from "../all-images/cars-img/toyota-offer-2.png";
+import img08 from "../all-images/cars-img/mercedes-offer.png";
+
+const carData = [
+  {
+    id: 1,
+    brand: "Tata Nexon",
+    rating: 112,
+    carName: "Tata Nexon ",
+    imgUrl: img01,
+    model: "Model 3",
+    price: 570,
+    speed: "120kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 2,
+    brand: "Hyundai",
+    rating: 102,
+    carName: "Hyundai Creta",
+    imgUrl: img02,
+    model: "Model-2024",
+    price: 990,
+    speed: "150kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 3,
+    brand: "Maruti",
+    rating: 132,
+    carName: "Maruti Swift",
+    imgUrl: img03,
+    model: "Model-2024",
+    price: 400,
+    speed: "120kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 4,
+    brand: "Nissan",
+    rating: 102,
+    carName: "Nissan ",
+    imgUrl: img04,
+    model: "Model-2024",
+    price: 1170,
+    speed: "220kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 5,
+    brand: "Ferrari",
+    rating: 94,
+    carName: "Ferrari Camry",
+    imgUrl: img05,
+    model: "Model-2024",
+    price: 2245,
+    speed: "320kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 6,
+    brand: "Mercedes",
+    rating: 119,
+    carName: "Mercedes Benz XC90",
+    imgUrl: img06,
+    model: "Model-2024",
+    price: 1185,
+    speed: "220kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 7,
+    brand: "Audi",
+    rating: 82,
+    carName: "Audi Fiesta",
+    imgUrl: img07,
+    model: "Model 3",
+    price: 1550,
+    speed: "320kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 8,
+    brand: "Colorado",
+    rating: 52,
+    carName: "Rolls Royce Colorado",
+    imgUrl: img08,
+    model: "Model 2024",
+    price: 1050,
+    speed: "290kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+];
+
+export default carData;
